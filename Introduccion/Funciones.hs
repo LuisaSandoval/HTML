f62137672f03092e1a0733254d25a2abe4d37299
LuisaSandoval <72033699+LuisaSandoval@users.noreply.github.com>
2020-11-11 22:07:11 -0700

    Add files via upload

---
+++ Introduccion/Funciones.hs
@@ -0,0 +1,5 @@
+suma a b = a + b
+resta a b = a - b
+sumar a b c d e = a + b + c + d + e
+division a b = a `div`  b
+pregunta a = not a
---
+++ Introduccion/Practica1.hs
@@ -0,0 +1,26 @@
+suma1 a b c= a + b + c
+suma2 a b c d e = a + b + c + d + e
+
+operado1 a b = a + b
+operador11 a b c d = a * b * c * d
+
+operador2 a b = a `div`  b
+operador22 a b = b `div` a
+
+operador3 a b = a `mod` b
+operador33 a b = b `mod` a
+
+operador4 a = True 
+operador44 a = False 
+
+operador5 a b c = a && b && c
+operador55 a b = b && a
+
+operador6 a b = a == b
+operador66 a b c = a + b /= c
+
+operador7 a = not a
+operador77 a b = not b
+
+operador8 a b = a ++ b
+operador a b c = a ++ b ++ c
